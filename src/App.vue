c752ea0099533965b41710b1df93beeeb098f934
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2022-03-31 00:08:16 +0200

    Fix heroku style.

--- src/App.vue
+++ src/App.vue
@@ -180,9 +180,15 @@ ul > li {
 }
 
 h1 {
-  font-size: 4.2rem;
-  line-height: 110%;
-  margin: 2.8rem 0 1.68rem 0;
+  font-size: 4.2rem !important; /* Need !mportant because of heroku style */
+  line-height: 110% !important; /* Need !mportant because of heroku style */
+  margin: 2.8rem 0 1.68rem 0 !important;  /* Need !mportant because of heroku style */
+}
+
+h3 {
+  font-size: 2.92rem !important; /* Need !mportant because of heroku style */
+  line-height: 110% !important; /* Need !mportant because of heroku style */
+  margin: 1.9466666667rem 0 1.168rem 0 !important; /* Need !mportant because of heroku style */
 }
 
 @media only screen and (max-width: 550px) {
